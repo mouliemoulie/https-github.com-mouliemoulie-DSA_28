05da640db9836a9de63d8a32ec7fd273e65dd8f4
mouliemoulie <mouliemoulie88@gmail.com>
2025-03-15 22:56:50 +0530

    Time: 3 ms (60.62%), Space: 7.8 MB (70.04%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int countOdds(int l, int h) {
-        int cnt=0;
-        for(int i=l;i<=h;i++){
-            if(i%2!=0) cnt+=1;
-        }
-        return cnt;
+        return ((h + 1) / 2) - (l / 2);
     }
 };
