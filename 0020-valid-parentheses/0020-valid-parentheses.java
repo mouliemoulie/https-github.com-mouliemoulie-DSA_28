c8c5b04c31c4cb3926c724b122628662cc30c369
mouliemoulie <mouliemoulie88@gmail.com>
2025-05-31 19:15:23 +0530

    Time: 2 ms (97.19%), Space: 41.6 MB (91.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -15,7 +15,13 @@ class Solution {
                 return false;
             }
         }
-        return st.isEmpty();
-        
+        return st.isEmpty(); 
+       /* int cnt=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='[' || ch=='{') cnt+=1;
+            else if (ch==')'|| ch==']' || ch=='}') cnt-=1;
+            if(cnt<0) return false;
+        }
+        return true; */
     }
 }
