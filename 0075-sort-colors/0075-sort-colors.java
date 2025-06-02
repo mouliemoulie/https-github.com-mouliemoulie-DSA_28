1bc59c7b1085507687b501d0894a932affbf282e
mouliemoulie <mouliemoulie88@gmail.com>
2025-06-02 12:06:36 +0530

    Time: 2 ms (10.59%), Space: 44 MB (9.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) { 
-         int count0 = 0, count1 = 0, count2 = 0;
+        /*int count0 = 0, count1 = 0, count2 = 0;
 
         // Step 1: Count occurrences of 0, 1, and 2
         for (int num : nums) {
@@ -13,6 +13,24 @@ class Solution {
         int i = 0;
         while (count0-- > 0) nums[i++] = 0;
         while (count1-- > 0) nums[i++] = 1;
-        while (count2-- > 0) nums[i++] = 2;
+        while (count2-- > 0) nums[i++] = 2; */
+        bs(nums,nums.length,0);
+
+    }
+    public static void bs(int[] arr,int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(c+1<r && arr[c]>arr[c+1]){
+                int t=arr[c+1];
+                arr[c+1]=arr[c];
+                arr[c]=t;
+            }
+            bs(arr,r,c+1);
+        }
+        else{
+            bs(arr,r-1,0);
+        }
     }
 }
