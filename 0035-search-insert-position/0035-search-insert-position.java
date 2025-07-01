25518553f8704074bbdeb9c9ebab86cb7480e450
mouliemoulie <mouliemoulie88@gmail.com>
2025-07-01 08:11:23 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.29%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -6,11 +6,19 @@ class Solution {
         while (start <= end) {
             int mid = start + (end-start)/2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
+            if (nums[mid] > target) end = mid-1;
             else start = mid+1;
         }
 
         return start;
+        /*int i=0;
+        int l=nums.length;
+        int indx=-1;
+        while(i<l){
+            if(nums[i]==t) return i;
+            else if(i+1<l && t>nums[i] && t<nums[i+1]) indx=i+1;
+            else if(t<nums[i] && t<nums[i+1])
+        }*/
 
     }
 }
