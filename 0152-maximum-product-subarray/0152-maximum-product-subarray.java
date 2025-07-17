3e73a90dee0dded2c53d67e95d8d986ba5806a8e
mouliemoulie <mouliemoulie88@gmail.com>
2025-07-17 21:03:57 +0530

    Time: 0 ms (100%), Space: 45.1 MB (43.9%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -14,6 +14,17 @@ class Solution {
             max2=Math.max(product,max2);
             if(product==0) product=1;
         }
-        return Math.max(max1,max2);
+        return Math.max(max1,max2); 
+       /* int l=0,r=0;
+        int maxproduct=Integer.MIN_VALUE;
+        while(r<nums.length){
+            product*=nums[r];
+            if(r+1<nums.length && nums[r]<=0){
+                l=r+1;
+            }
+            maxproduct=Math.max(product,maxproduct);
+            r++;
+        }
+        return maxproduct;*/
     }
 }
