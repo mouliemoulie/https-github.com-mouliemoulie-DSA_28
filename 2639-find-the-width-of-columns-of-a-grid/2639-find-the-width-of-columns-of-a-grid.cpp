c92f226eb86a734a58cde20d794dcd9e51db04ad
mouliemoulie <mouliemoulie88@gmail.com>
2025-08-09 10:46:56 +0530

    Time: 7 ms (14.55%), Space: 15.6 MB (6.34%) - LeetHub

--- 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.cpp
+++ 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> findColumnWidth(vector<vector<int>>& grid) {
-        vector<int> res;
+       
         int r=grid.size();
         int c=grid[0].size();
+         vector<int> res(c,0);
         for(int i=0;i<c;i++){
             int mx=-1;
             for(int j=0;j<r;j++){
-                mx=max(helper(grid[j][i]),mx);
+                int len=to_string(grid[j][i]).size();
+                mx=max(mx,len);
             }
-            res.push_back(mx);
+            res[i]=mx;
         }
         return res;
-    }
-    int helper(int k){
-        int len=0;
-        if(k<0){
-            len=to_string(k).size();
-        }
-        else{
-            len=to_string(k).size();
-        }
-        return len;
     }
 };
