a65cb6ec0b52d5bb173bb630739bbb4cac168795
mouliemoulie <mouliemoulie88@gmail.com>
2025-08-11 12:04:36 +0530

    Time: 3 ms (68.65%), Space: 12.6 MB (36.91%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> res;
+        /*vector<int> res;
         for(int i=0;i<boxes.size();i++){
             int opmov=0;
             for(int j=0;j<boxes.size();j++){
@@ -10,6 +10,24 @@ public:
                 }
             }
             res.push_back(opmov);
+        }*/
+        int n=boxes.size();
+        vector<int> leftcost(n,0),rightcost(n,0),res(n,0);
+        int ball=0,opcost=0;
+        for(int i=0;i<boxes.size();i++){
+            leftcost[i]=opcost;
+            if(boxes[i]!='0') ball++;
+            opcost+=ball;
+        }
+        ball=0;
+        opcost=0;
+        for(int i=boxes.size()-1;i>=0;i--){
+            rightcost[i]=opcost;
+            if(boxes[i]!='0') ball++;
+            opcost+=ball;
+        }
+        for(int i=0;i<boxes.size();i++){
+           res[i]=leftcost[i]+rightcost[i];
         }
         return res;
     }
